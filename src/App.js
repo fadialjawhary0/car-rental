c594a869338c45ad8ba4faeac0a2fd1e5e6069ad
fadialjohari0 <fadialjohari0@gmail.com>
2024-05-25 23:09:08 +0300

    Setup pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './views/HomePage/Home';
+import About from './views/AboutPage/About';
+import Models from './views/ModelsPage/Models';
+import Testimonials from './views/TestimonialsPage/Testimonials';
+import Team from './views/TeamPage/Team';
+import Contact from './views/ContactPage/Contact';
+
 function App() {
   return (
-    <div>
-      <header>Hi</header>
-    </div>
+    <>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="About" element={<About />} />
+        <Route path="Models" element={<Models />} />
+        <Route path="Testimonials" element={<Testimonials />} />
+        <Route path="Team" element={<Team />} />
+        <Route path="Contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
